0bb91eb94f27aab398130afc5a99bc402c16b89c
Salvydas Lukosius <sall@w-ss.io>
2021-12-11 06:43:15 +0000

    renamed:    crasis -> functions/crasis 	modified:   zi-crasis.plugin.zsh

--- zi-crasis.plugin.zsh
+++ zi-crasis.plugin.zsh
@@ -20,16 +20,6 @@ if [[ "${+functions[-zui_std_cleanup]}" = "0" ]]; then
   echo "Crasis uses ZUI plugin, please load https://github.com/z-shell/zui/ with your plugin manager, or source it."
 fi
 
-#
-# Update FPATH if:
-# 1. Not loading with ZI
-# 2. Not having fpath already updated (that would equal: using other plugin manager)
-#
-
-#if [[ -z "$ZPLG_CUR_PLUGIN" && "${fpath[(r)$ZCRASIS_REPO_DIR]}" != $ZCRASIS_REPO_DIR ]]; then
-#  fpath+=( "$ZCRASIS_REPO_DIR" )
-#fi
-
 [[ -z "${fg_bold[green]}" ]] && builtin autoload -Uz colors && colors
 
 autoload -- crasis -zcrasis-process-buffer
