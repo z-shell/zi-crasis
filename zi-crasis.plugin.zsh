9b074fb1f456e5219328c7e49284a1efbd8af96b
Salvydas Lukosius <sall@w-ss.io>
2021-12-11 07:21:25 +0000

    zi-crasis 0d4e7ff > 2ae1de2 (#2)
    * renamed:    functions/crasis -> crasis
    
    * renamed:    crasis -> functions/crasis
    
    * renamed:    functions/-zcrasis-process-buffer -> -zcrasis-process-buffer
    	renamed:    functions/crasis -> crasis
      modified:   gold.lst
      modified:   layouts/default.cr-layout
      modified:   zi-crasis.plugin.zsh
    
    * modified:   crasis
    	modified:   zi-crasis.plugin.zsh
    
    * modified:   gold.lst
    
    * renamed:    -zcrasis-process-buffer -> functions/-zcrasis-process-buffer
    	renamed:    crasis -> functions/crasis
    	modified:   zi-crasis.plugin.zsh
    
    * modified:   zi-crasis.plugin.zsh
    
    * modified:   Makefile
    	renamed:    functions/crasis -> crasis
    	modified:   zi-crasis.plugin.zsh
    
    * renamed:    crasis -> functions/crasis
    	modified:   zi-crasis.plugin.zsh
    
    * modified:   .editorconfig
    	modified:   Makefile
    	new file:   docs/man/crasis.1
    	renamed:    zsdoc/pdf/crasis.pdf -> docs/pdf/crasis.pdf
    	modified:   zi-crasis.plugin.zsh
    	modified:   zsdoc/crasis.adoc
    
    * modified:   Makefile
    	deleted:    zsdoc/Makefile
    	deleted:    zsdoc/crasis.adoc
    
    * modified:   zi-crasis.plugin.zsh

--- zi-crasis.plugin.zsh
+++ zi-crasis.plugin.zsh
@@ -1,14 +1,11 @@
+#
 # No plugin manager is needed to use this file. All that is needed is adding:
 #   source {where-crasis-is}/zi-crasis.plugin.zsh
 #
 # to ~/.zshrc.
 #
-
-# Standarized way of handling finding plugin dir,
-# regardless of functionargzero and posixargzero,
-# and with an option for a plugin manager to alter
-# the plugin directory (i.e. set ZERO parameter)
-#
+# Standarized way of handling finding plugin dir, regardless of functionargzero and posixargzero,
+# and with an option for a plugin manager to alter the plugin directory (i.e. set ZERO parameter)
 # https://github.com/z-shell/zi/wiki/Zsh-Plugin-Standard
 
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
@@ -18,7 +15,7 @@ if [[ $PMSPEC != *f* ]] {
   fpath+=( "${0:h}/functions" )
 }
 
-typeset -g ZCRASIS_DIR="${0:h}"
+typeset -g ZCRASIS_REPO_DIR="${0:h}"
 
 if [[ "${+functions[-zui_std_cleanup]}" = "0" ]]; then
   echo "Crasis uses ZUI plugin, please load https://github.com/z-shell/zui/ with your plugin manager, or source it."
@@ -30,3 +27,5 @@ autoload -- crasis -zcrasis-process-buffer
 
 zle -N crasis
 bindkey "^O^K" crasis
+
+# vim:ft=zsh
