5b03ec6fd23715956b54a3b0f9c05820f0133135
Salvydas Lukosius <sall@w-ss.io>
2021-12-11 05:46:14 +0000

    renamed:    functions/-zcrasis-process-buffer -> -zcrasis-process-buffer 	renamed:    functions/crasis -> crasis   modified:   gold.lst   modified:   layouts/default.cr-layout   modified:   zi-crasis.plugin.zsh

--- zi-crasis.plugin.zsh
+++ zi-crasis.plugin.zsh
@@ -1,27 +1,25 @@
+#
 # No plugin manager is needed to use this file. All that is needed is adding:
 #   source {where-crasis-is}/zi-crasis.plugin.zsh
 #
 # to ~/.zshrc.
 #
 
-# Standarized way of handling finding plugin dir,
-# regardless of functionargzero and posixargzero,
-# and with an option for a plugin manager to alter
-# the plugin directory (i.e. set ZERO parameter)
-#
-# https://github.com/z-shell/zi/wiki/Zsh-Plugin-Standard
-
-0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
-0="${${(M)0:#/*}:-$PWD/$0}"
-
-if [[ $PMSPEC != *f* ]] {
-  fpath+=( "${0:h}/functions" )
-}
-
-typeset -g ZCRASIS_DIR="${0:h}"
+0="${(%):-%N}" # this gives immunity to functionargzero being unset
+export ZCRASIS_REPO_DIR="${0%/*}"
 
 if [[ "${+functions[-zui_std_cleanup]}" = "0" ]]; then
-  echo "Crasis uses ZUI plugin, please load https://github.com/z-shell/zui/ with your plugin manager, or source it."
+    echo "Crasis uses ZUI plugin, please load https://github.com/z-shell/zui/ with your plugin manager, or source it."
+fi
+
+#
+# Update FPATH if:
+# 1. Not loading with ZI
+# 2. Not having fpath already updated (that would equal: using other plugin manager)
+#
+
+if [[ -z "$ZPLG_CUR_PLUGIN" && "${fpath[(r)$ZCRASIS_REPO_DIR]}" != $ZCRASIS_REPO_DIR ]]; then
+    fpath+=( "$ZCRASIS_REPO_DIR" )
 fi
 
 [[ -z "${fg_bold[green]}" ]] && builtin autoload -Uz colors && colors
@@ -30,3 +28,5 @@ autoload -- crasis -zcrasis-process-buffer
 
 zle -N crasis
 bindkey "^O^K" crasis
+
+# vim:ft=zsh
