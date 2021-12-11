5abd0c68571090e8b0922d27a1112f352edf3d13
Salvydas Lukosius <sall@w-ss.io>
2021-12-11 06:17:24 +0000

    renamed:    -zcrasis-process-buffer -> functions/-zcrasis-process-buffer 	renamed:    crasis -> functions/crasis 	modified:   zi-crasis.plugin.zsh

--- zi-crasis.plugin.zsh
+++ zi-crasis.plugin.zsh
@@ -4,9 +4,17 @@
 #
 # to ~/.zshrc.
 #
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
 
-0="${(%):-%N}" # this gives immunity to functionargzero being unset
-export ZCRASIS_REPO_DIR="${0%/*}"
+if [[ $PMSPEC != *f* ]] {
+  fpath+=( "${0:h}/functions" )
+}
+
+typeset -g ZCRASIS_DIR="${0:h}"
+
+#0="${(%):-%N}" # this gives immunity to functionargzero being unset
+#export ZCRASIS_REPO_DIR="${0%/*}"
 
 if [[ "${+functions[-zui_std_cleanup]}" = "0" ]]; then
   echo "Crasis uses ZUI plugin, please load https://github.com/z-shell/zui/ with your plugin manager, or source it."
@@ -18,9 +26,9 @@ fi
 # 2. Not having fpath already updated (that would equal: using other plugin manager)
 #
 
-if [[ -z "$ZPLG_CUR_PLUGIN" && "${fpath[(r)$ZCRASIS_REPO_DIR]}" != $ZCRASIS_REPO_DIR ]]; then
-  fpath+=( "$ZCRASIS_REPO_DIR" )
-fi
+#if [[ -z "$ZPLG_CUR_PLUGIN" && "${fpath[(r)$ZCRASIS_REPO_DIR]}" != $ZCRASIS_REPO_DIR ]]; then
+#  fpath+=( "$ZCRASIS_REPO_DIR" )
+#fi
 
 [[ -z "${fg_bold[green]}" ]] && builtin autoload -Uz colors && colors
 
